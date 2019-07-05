e3dcd277b3ab51c4e07a19fd4f332e91cadd7feb
pinct <51133746+pinct@users.noreply.github.com>
2019-07-04 22:28:54 -0400

    7/4

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -6,10 +6,12 @@ public class EnemyMove : MonoBehaviour
 {
 	public GameObject Player;
 	public GameObject Enemy;
-	public float speed;
+	public float speed = 0.00f;
 	public int MoveTrigger;
+	public int ShootTrigger;
 	public RaycastHit shot;
 	public float TargetDistance;
+	public int IsShooting = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,24 +22,42 @@ public class EnemyMove : MonoBehaviour
     void Update()
     {
 		Enemy.GetComponent<Animator>().SetInteger("MoveTrigger", MoveTrigger);
+		Enemy.GetComponent<Animator>().SetInteger("ShootTrigger", ShootTrigger);
 		if (Player != null)
 		{
 			Enemy.transform.LookAt(Player.transform);
 			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
 			{
 				TargetDistance = shot.distance;
-				if (TargetDistance < 10)
+				if (shot.transform.tag == "Player")
 				{
-					speed = 0.04f;
-					Enemy.transform.position = Vector3.MoveTowards(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y - 1.1f, Player.transform.position.z), speed);
-					MoveTrigger = 1;
-				}
-				else
-				{
-					speed = 0.00f;
-					MoveTrigger = 0;
+					if (TargetDistance > 20)
+					{
+						speed = 0.02f;
+						Enemy.transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed);
+						MoveTrigger = 1;
+						ShootTrigger = 0;
+					}
+					else
+					{
+						speed = 0.00f;
+						MoveTrigger = 0;
+						ShootTrigger = 1;
+						if (IsShooting == 0)
+						{
+							StartCoroutine(EnemyDamage());
+						}
+					}
 				}
 			}
 		}
     }
+	IEnumerator EnemyDamage()
+	{
+		IsShooting = 1;
+		yield return new WaitForSeconds(1.0f/30.0f);
+		GlobalHealth.PlayerHealth -= 1;
+		yield return new WaitForSeconds(29.0f/30.0f);
+		IsShooting = 0;
+	}
 }
